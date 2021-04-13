f981c39d0bd9c4c996ac9fc28d898e6509116120
Thales Domingues <thdomingues.dev@gmail.com>
2021-04-12 21:51:50 -0400

    feat(themes): created light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import light from './styles/themes/light';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle />
-      <Header />
-    </div>
+    <ThemeProvider theme={light}>
+      <div className="App">
+        <GlobalStyle />
+        <Header />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,13 @@
+const light = {
+  title: 'light',
+
+  colors: {
+    primary: '#AD343E',
+    secundary: '#F2AF29',
+
+    background: '#f0f0f0',
+    text: '#333'
+  },
+};
+
+export default light;
