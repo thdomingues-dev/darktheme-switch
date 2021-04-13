467af6ed09fb4fc9f0333bf7e4252ab62fb9edd0
Thales Domingues <thdomingues.dev@gmail.com>
2021-04-12 21:30:21 -0400

    feat(header): created component styles

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 4.3rem;
+  background: #7159c1;
+  color: #FFF;
+  display: flex;
+  align-items: center;
+  padding: 0 2.15rem;
+`;
